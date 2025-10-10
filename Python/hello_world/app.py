18f112235e2ecc6614c0840b511ccd5cb2215e37
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-10-10 08:36:36 +0530

    4th commit

--- Python/hello_world/app.py
+++ Python/hello_world/app.py
@@ -1,5 +1,7 @@
-course = "Python Programmming"
+course = "  Python Programmming"
 
 print(course.upper())
 print(course.lower())
 print(course.title())
+print(course.strip())
+print(course.find("Pro"))
