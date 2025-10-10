c2eefcd32d6bb20d5214be25f115bae02e4e178f
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-10-10 08:23:24 +0530

    2nd commit

--- Python/hello_world/app.py
+++ Python/hello_world/app.py
@@ -1,9 +1,3 @@
-course = "Python Programmming"
+course = "Python \nProgrammming"
 
-print (len(course))
-print (course[0])
-print (course[-1])
-print (course[0:3])
-print (course[-1])
-print (course[-1])
-print (course[-1])
+print (course)
