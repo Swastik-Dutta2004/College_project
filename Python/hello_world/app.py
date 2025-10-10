18eb1237c13a5925b7888b76b645c2d389e8689b
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-10-10 10:32:17 +0530

    6th commit

--- Python/hello_world/app.py
+++ Python/hello_world/app.py
@@ -1,10 +1,3 @@
-course = "  Python Programmming"
-
-print(course.upper())
-print(course.lower())
-print(course.title())
-print(course.strip())
-print(course.find("Pro"))
-print (course.replace("p", "j"))
-print("Pro" in course)
-print("swift" in course)
+X = input("x: ")
+y = int(X) + 1
+print(f"x: {X}, y: {y}")
