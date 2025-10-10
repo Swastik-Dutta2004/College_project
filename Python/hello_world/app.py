5bd04940ef5ac9aeb2bdb574c0b5170fe0a6422c
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-10-10 11:01:34 +0530

    8th commit

--- Python/hello_world/app.py
+++ Python/hello_world/app.py
@@ -1,9 +1,19 @@
-temprature = 19
-if temprature > 30:
-    print("Its warm")
-    print("Drink water")
-elif temprature > 20:
-    print("Its a nice weather")
-else:
-    print("Winter has came")
-print("Done")
+# temprature = 19
+# if temprature > 30:
+#     print("Its warm")
+#     print("Drink water")
+# elif temprature > 20:
+#     print("Its a nice weather")
+# else:
+#     print("Winter has came")
+# print("Done")
+
+
+age = 17
+# if age > 18:
+#     print("Eligible")
+# else:
+#     print("Not Eligible")
+
+message = "Eligible" if age >= 18 else "Not eligible"
+print(message)
